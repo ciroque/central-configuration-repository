002310ac861db4439fc211e352dd9e6105e31e19
Steve <ciroque@live.com>
2015-06-30 17:19:40 -0700

    Adding value assertions to tests

--- src/test/scala/org/ciroque/ccr/SettingSpec.scala
+++ src/test/scala/org/ciroque/ccr/SettingSpec.scala
@@ -3,6 +3,7 @@ package org.ciroque.ccr
 import org.ciroque.ccr.models.Setting
 import org.joda.time.{DateTimeZone, DateTime}
 import org.specs2.mutable.Specification
+import spray.json.{JsNumber, JsValue, JsString}
 
 class SettingSpec extends Specification {
 
@@ -23,12 +24,20 @@ class SettingSpec extends Specification {
     }
 
     "be renderable to JSON" in {
+
+      def assertValue(map: Map[String, JsValue], key: String, expectedValue: JsValue) = {
+        map.get(key).map(actualValue => actualValue must_== expectedValue)
+      }
+
       val setting = Setting("env", "app", "scope", "setting", "1000000", effectiveAt, expiresAt, 5000)
       val json = setting.toJson.asJsObject
+
       val fields = json.fields
       fields.keys must contain("key")
       fields.keys must contain("value")
       fields.keys must contain("temporality")
+
+      assertValue(fields, "value", JsString("1000000"))
 
       val keyFields = fields.get("key").get.asJsObject.fields
       keyFields.keys must contain("environment")
@@ -36,13 +45,21 @@ class SettingSpec extends Specification {
       keyFields.keys must contain("scope")
       keyFields.keys must contain("setting")
 
-      val value = fields.get("value").get
-      value.toString must_== "\"1000000\""
+      assertValue(keyFields, "environment", JsString("env"))
+      assertValue(keyFields, "application", JsString("app"))
+      assertValue(keyFields, "scope", JsString("scope"))
+      assertValue(keyFields, "setting", JsString("setting"))
 
       val temporalityFields = fields.get("temporality").get.asJsObject.fields
       temporalityFields.keys must contain("effectiveAt")
       temporalityFields.keys must contain("expiresAt")
       temporalityFields.keys must contain("ttl")
+
+//      assertValue(temporalityFields, "effectiveAt", JsString(effectiveAt.toString))
+//      assertValue(temporalityFields, "expiresAt", JsString(expiresAt.toString))
+      assertValue(temporalityFields, "ttl", JsNumber(5000))
+
+      true must_== true
     }
   }
 }
