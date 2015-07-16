fcd2f9b3807ed79e9f2950a5e33482ac5b73bdad
Steve <ciroque@live.com>
2015-07-16 11:09:57 -0700

    Implementing InMemoryDataStore more fully.

--- src/main/scala/org/ciroque/ccr/datastores/InMemoryDataStore.scala
+++ src/main/scala/org/ciroque/ccr/datastores/InMemoryDataStore.scala
@@ -5,21 +5,53 @@ import org.ciroque.ccr.core.{DataStoreResults, SettingsDataStore}
 import org.ciroque.ccr.models.ConfigurationFactory.Configuration
 
 class InMemoryDataStore extends SettingsDataStore {
-  var environments: List[String] = List()
+
+  var environments: Map[String, Map[String, Map[String, List[String]]]] = Map()
 
   override def createEnvironment(environment: String): DataStoreResult =  {
     if(!environments.contains(environment)) {
-      environments = environments :+ environment
+      environments += (environment -> Map())
     }
     DataStoreResults.Success()
   }
-  override def createApplication(environment: String, application: String): DataStoreResult = Failure("Not Implemented")
-  override def createScope(environment: String, application: String, scope: String): DataStoreResult = Failure("Not Implemented")
+
+  override def createApplication(environment: String, application: String): DataStoreResult = {
+    if(environments.contains(environment)) {
+      var applications = environments.apply(environment)
+      if(!applications.contains(application)) {
+        applications += (application -> Map())
+        environments += (environment -> applications)
+      }
+    } else {
+      DataStoreResults.Failure(s"Environment '$environment' was not found.")
+    }
+
+    println(environments)
+    DataStoreResults.Success()
+  }
+
+  override def createScope(environment: String, application: String, scope: String): DataStoreResult = {
+    if(environments.contains(environment)) {
+      var applications = environments.apply(environment)
+      if(applications.contains(application)) {
+        var scopes = applications.apply(application)
+        if(!scopes.contains(scope)) {
+          scopes += (scope -> List())
+          applications += (application -> scopes)
+        }
+        environments += (environment -> applications)
+      }
+    }
+    println(environments)
+    DataStoreResults.Success()
+  }
+
+
   override def upsertConfiguration(environment: String, application: String, scope: String, setting: String, configuration: Configuration): DataStoreResult = Failure("Not Implemented")
 
   override def retrieveEnvironments(): InterstitialResultOption = {
     println(s"InMemoryDataStore::retrieveEnvironments($environments)")
-    Some(environments)
+    Some(environments.keys.toList)
   }
   override def retrieveApplications(environment: String): InterstitialResultOption = {
     println(s"InMemoryDataStore::retrieveApplications($environment)")
