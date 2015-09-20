7425ac1efb0598fed60e47f46b54bf5702825305
Steve Wagner <scalawagz@outlook.com>
2015-09-20 11:52:29 -0700

    Results are now logged by ImplicitLogging. Closes #14

--- src/main/scala/org/ciroque/ccr/logging/ImplicitLogging.scala
+++ src/main/scala/org/ciroque/ccr/logging/ImplicitLogging.scala
@@ -17,12 +17,19 @@ object ImplicitLogging {
 
     try {
       fx
-
     } finally {
       val ended = DateTime.now(DateTimeZone.UTC)
       val logEntry = getCurrentImplicitLogger.buildAsJson(name, started, ended)
       logger.info(logEntry.toString())
     }
+  }
+
+  def setResultException(cause: Throwable) = {
+    getCurrentImplicitLogger.result = Exception(cause.getMessage, cause)
+  }
+
+  def setResultError(msg: String) = {
+    getCurrentImplicitLogger.result = Error(msg)
   }
 
   def recordValue(name: String, value: String) = {
@@ -31,6 +38,7 @@ object ImplicitLogging {
 
   class LogEntryBuilder() {
     var values: List[Values] = List()
+    var result: Result = Success()
 
     def addValue(name: String, value: String): List[Values] = {
       values = values :+ Values(name, value)
@@ -38,7 +46,7 @@ object ImplicitLogging {
     }
 
     def build(name: String, started: DateTime, ended: DateTime): LogEntry = {
-      LogEntry(name, Timing(started, ended, ended.getMillis - started.getMillis), values)
+      LogEntry(name, Timing(started, ended, ended.getMillis - started.getMillis), values, result)
     }
 
     def buildAsJson(name: String, started: DateTime, ended: DateTime): JsValue = {
@@ -55,23 +63,47 @@ object ImplicitLogging {
     implicit def timingFormat: RootJsonFormat[Timing] = jsonFormat3(Timing)
   }
 
-  object LogEntryProtocol extends DefaultJsonProtocol {
+  implicit object ResultJsonFormat extends RootJsonFormat[Result] {
+    override def read(json: JsValue): Result = {
+      val jso = json.asJsObject
+      jso.fields.size match {
+        case 0 => Success()
+        case 1 => Error(jso.fields("msg").toString())
+        case 2 => Exception(jso.fields("msg").toString(), null)
+      }
+    }
 
+    override def write(result: Result): JsValue = {
+      result match {
+        case success: Success => JsObject("status" -> JsString("SUCCESS"))
+        case error: Error => JsObject("status" -> JsString("ERROR"), "message" -> JsString(error.msg))
+        case exception: Exception =>
+          JsObject(
+            "status" -> JsString("EXCEPTION"),
+            "message" -> JsString(exception.cause.getMessage),
+            "stackTrace" -> JsString(exception.cause.getStackTrace.mkString))
+      }
+    }
+  }
+
+  object LogEntryProtocol extends DefaultJsonProtocol {
     import org.ciroque.ccr.logging.ImplicitLogging.TimingProtocol._
     import org.ciroque.ccr.logging.ImplicitLogging.ValuesProtocol._
 
-    implicit def logEntryFormat: RootJsonFormat[LogEntry] = jsonFormat3(LogEntry)
+    implicit def logEntryFormat: RootJsonFormat[LogEntry] = jsonFormat4(LogEntry)
   }
 
-  case class LogEntry(name: String, timing: Timing, values: List[Values])
+  case class LogEntry(name: String, timing: Timing, values: List[Values], result: Result = Success())
 
   case class Timing(start: DateTime, end: DateTime, duration: Long)
 
   case class Values(name: String, value: String)
 
-  //  private trait Result(name: String)
-  //
-  //  private case class Success() extends Result
-  //  private case class Error(msg: String) extends Result
-  //  private case class Exception(msg: String, cause: Throwable)
+  trait Result
+
+  case class Success() extends Result
+
+  case class Error(msg: String) extends Result
+
+  case class Exception(msg: String, cause: Throwable) extends Result
 }
--- src/test/scala/org/ciroque/ccr/logging/ImplicitLoggingTests.scala
+++ src/test/scala/org/ciroque/ccr/logging/ImplicitLoggingTests.scala
@@ -7,7 +7,7 @@ import org.scalatest.{FunSpec, Matchers}
 class ImplicitLoggingTests extends FunSpec with Matchers with EasyMockSugar {
 
   describe("ImplicitLogging") {
-    it("writes an info message to the logger") {
+    it("writes an info message to the logger with Success result") {
       implicit val mockLogger = new CachingLogger()
       val implicitLoggerName: String = "IMPLICIT_LOGGING_TESTING"
       withImplicitLogging(implicitLoggerName) {
@@ -24,6 +24,61 @@ class ImplicitLoggingTests extends FunSpec with Matchers with EasyMockSugar {
       firstEvent should include(implicitLoggerName)
       firstEvent should include("Value1")
       firstEvent should include("TheValueOfOne")
+      firstEvent should include("SUCCESS")
+
+      mockLogger.printLog()
+    }
+
+    it("writes an info message to the logger with Error result") {
+      implicit val mockLogger = new CachingLogger()
+      val implicitLoggerName: String = "IMPLICIT_LOGGING_TESTING"
+      withImplicitLogging(implicitLoggerName) {
+        implicit val logger = mockLogger
+
+        ()
+
+        recordValue("Value1", "TheValueOfOne")
+        setResultError("ERROR_MESSAGE")
+      }
+
+      mockLogger.getEvents.size shouldBe 1
+      val firstEvent = mockLogger.getEvents.head
+
+      firstEvent should include(implicitLoggerName)
+      firstEvent should include("Value1")
+      firstEvent should include("TheValueOfOne")
+      firstEvent should include("ERROR")
+      firstEvent should include("ERROR_MESSAGE")
+
+      mockLogger.printLog()
+    }
+
+    it("writes an info message to the logger with Exception result") {
+      implicit val mockLogger = new CachingLogger()
+      val implicitLoggerName: String = "IMPLICIT_LOGGING_TESTING"
+
+      withImplicitLogging(implicitLoggerName) {
+        try {
+          implicit val logger = mockLogger
+
+          recordValue("Value1", "TheValueOfOne")
+
+          throw new IllegalArgumentException("AN_EXCEPTION_MESSAGE")
+        } catch {
+          case ex: Throwable => setResultException(ex)
+        }
+      }
+
+      mockLogger.getEvents.size shouldBe 1
+      val firstEvent = mockLogger.getEvents.head
+
+      firstEvent should include(implicitLoggerName)
+      firstEvent should include("Value1")
+      firstEvent should include("TheValueOfOne")
+      firstEvent should include("EXCEPTION")
+      firstEvent should include("AN_EXCEPTION_MESSAGE")
+
+      mockLogger.printLog()
     }
   }
 }
