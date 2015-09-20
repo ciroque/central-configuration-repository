c2798e55f4b45f5e9346df664a3672381b3f35a6
Steve Wagner <scalawagz@outlook.com>
2015-09-20 09:26:54 -0700

    Redis failure will not stop the process.

--- src/main/scala/org/ciroque/ccr/stats/RedisAccessStatsClient.scala
+++ src/main/scala/org/ciroque/ccr/stats/RedisAccessStatsClient.scala
@@ -5,10 +5,10 @@ import com.redis.RedisClient
 import scala.concurrent.Future
 
 class RedisAccessStatsClient extends AccessStatsClient {
-  val client = new RedisClient("localhost", 6379)
 
   def recordQuery(environment: String, application: String, scope: String, setting: String): Future[Long] = {
     import scala.concurrent.ExecutionContext.Implicits.global
+    val client = new RedisClient("localhost", 6379)
     Future {
       client.incr(s"CCR-QUERY:$environment:$application:$scope:$setting") match {
         case Some(value) => value
