9b23ba167128e8aea88db7a059a290313afd2f8c
Steve <ciroque@live.com>
2015-11-02 19:44:01 -0800

    Redis access stats failing wont fail the call.

--- src/main/scala/org/ciroque/ccr/stats/RedisAccessStatsClient.scala
+++ src/main/scala/org/ciroque/ccr/stats/RedisAccessStatsClient.scala
@@ -5,15 +5,15 @@ import com.redis.RedisClient
 import scala.concurrent.Future
 
 class RedisAccessStatsClient extends AccessStatsClient {
-
+  val ERROR_CODE = -1
   def recordQuery(environment: String, application: String, scope: String, setting: String): Future[Long] = {
     import scala.concurrent.ExecutionContext.Implicits.global
-    val client = new RedisClient("localhost", 6379)
     Future {
+      val client = new RedisClient("localhost", 6379)
       client.incr(s"CCR-QUERY:$environment:$application:$scope:$setting") match {
         case Some(value) => value
-        case None => -1
+        case None => ERROR_CODE
       }
-    }
+    }.recoverWith { case t: Throwable => Future.successful(ERROR_CODE) }
   }
 }
--- src/test/scala/org/ciroque/ccr/AccessStatsClientTests.scala
+++ src/test/scala/org/ciroque/ccr/AccessStatsClientTests.scala
@@ -4,6 +4,8 @@ import com.redis.RedisClient
 import org.ciroque.ccr.stats.RedisAccessStatsClient
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers}
+import scala.util.{Success, Failure}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class AccessStatsClientTests
   extends FunSpec
@@ -19,14 +21,19 @@ class AccessStatsClientTests
   val setting = "test-setting"
 
   override def beforeAll(): Unit = {
-    val client = new RedisClient("localhost", 6379)
-    client.flushall
+    try {
+      val client = new RedisClient("localhost", 6379)
+      client.flushall
+    } catch {
+      case t: Throwable => println(s"ACK: Error connecting to Redis (IGNORING) $t")
+    }
   }
 
   describe("Query Stats") {
     it("adds a non-existent key and increments to 1") {
-      whenReady(accessStatsClient.recordQuery(environment, application, scope, setting)) {
-        value => value should be(1)
+      accessStatsClient.recordQuery(environment, application, scope, setting) onComplete {
+        case Success(v) => v should be(1)
+        case Failure(e) => fail(e)
       }
     }
   }
