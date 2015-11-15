ab8f8b0c63aa22a2f76237130d101d521f32c5bc
Steve <ciroque@live.com>
2015-11-15 11:55:37 -0800

    Adding an actor to wrap the Settings Data Store. Not sure of the advantage of this though.

--- src/main/scala/org/ciroque/ccr/datastores/SettingsDataStoreActor.scala
+++ src/main/scala/org/ciroque/ccr/datastores/SettingsDataStoreActor.scala
@@ -1,8 +1,23 @@
 package org.ciroque.ccr.datastores
 
-/**
-  * Created by steve on 11/14/15.
-  */
-class SettingsDataStoreActor {
+import akka.actor.Actor
+import org.ciroque.ccr.datastores.DataStoreResults.Found
+import org.ciroque.ccr.datastores.SettingsDataStoreActor._
 
+object SettingsDataStoreActor {
+  case class GetEnvironments()
+  case class GetApplications(environment: String)
+  case class GetScopes(environment: String, application: String)
+  case class GetSettings(environment: String, application: String, scope: String)
+  case class GetConfiguration(environment: String, application: String, scope: String, setting: String)
+}
+
+class SettingsDataStoreActor extends Actor {
+  override def receive: Receive = {
+    case GetEnvironments() => sender ! Found(List())
+    case GetApplications(environment) => sender ! Found(List())
+    case GetScopes(environment, application) => sender ! Found(List())
+    case GetSettings(environment, application, scope) => sender ! Found(List())
+    case GetConfiguration(environment, application, scope, setting) => sender ! Found(List())
+  }
 }
